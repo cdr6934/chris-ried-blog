9100fc1174c30c2d46611c448f6383b5328abb72
Christopher Ried <chris@cbayes.com>
2022-10-07 13:59:58 -0500

    Refactored blog.tsx to blog.js

---
+++ pages/post/[slug].js
@@ -0,0 +1,97 @@
+// [slug].js
+import groq from 'groq'
+import imageUrlBuilder from '@sanity/image-url'
+import {PortableText} from '@portabletext/react'
+import client from '../../client'
+import { Heading, Container, Text } from '@chakra-ui/react'
+
+function urlFor (source) {
+  return imageUrlBuilder(client).image(source)
+}
+
+const ptComponents = {
+  types: {
+    image: ({ value } ) => {
+      if (!value?.asset?._ref) {
+        return null
+      }
+      return (
+        <img
+          alt={value.alt || ' '}
+          loading="lazy"
+          src={String(urlFor(value).width(320).height(240).fit('max').auto('format'))}
+        />
+      )
+    }
+  }
+}
+
+
+
+const Post = ({post}) => {
+  const {
+    title = 'Missing title',
+    name = 'Missing name',
+    categories,
+    authorImage,
+    body = []
+  } = post
+  return (
+    <Container> 
+    <article>
+      <Heading>{title}</Heading>
+      <Text fontSize='lg'>By {name}</Text>
+      {categories && (
+        <ul>
+          Posted in
+          {categories.map((category) => <li key={category}>{category}</li>)}
+        </ul>
+      )}
+      {authorImage && (
+        <div>
+          <img
+            src={urlFor(authorImage)
+              .width(50)
+              .url()}
+            alt={`${name}'s picture`}
+          />
+        </div>
+      )}
+      <PortableText
+        value={body}
+        components={ptComponents}
+      />
+    </article>
+    </Container>
+  )
+}
+
+const query = groq`*[_type == "post" && slug.current == $slug][0]{
+  title,
+  "name": author->name,
+  "categories": categories[]->title,
+  "authorImage": author->image,
+  body
+}`
+export async function getStaticPaths() {
+  const paths = await client.fetch(
+    groq`*[_type == "post" && defined(slug.current)][].slug.current`
+  )
+
+  return {
+    paths: paths.map((slug) => ({params: {slug}})),
+    fallback: true,
+  }
+}
+
+export async function getStaticProps(context) {
+  // It's important to default the slug so that it doesn't return "undefined"
+  const { slug = "" } = context.params
+  const post = await client.fetch(query, { slug })
+  return {
+    props: {
+      post
+    }
+  }
+}
+export default Post
